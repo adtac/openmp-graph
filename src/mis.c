17ba37a155d83b0cb4f89b1b5c8ae3fc07f31009
Adhityaa <c.adhityaa@gmail.com>
2017-09-23 14:10:57 +0530

    mis: Modularize code and split main into functions

--- src/mis.c
+++ src/mis.c
@@ -18,25 +18,9 @@ double randnum() {
     return ((double) rand()) / ((double) RAND_MAX);
 }
 
-/**
- * Creates a MIS from a randomly generated graph in a distributed manner.
- *
- * Links:
- *   - https://disco.ethz.ch/courses/podc_allstars/lecture/chapter7.pdf
- *     See Algorithm 7.12 (Fast MIS-2)
- */
-int main(int argc, char* argv[]) {
-    int N = 16;
-    int M = 64;
-
-    if (argc > 1) {
-        sscanf(argv[1], "%d", &N);
-        sscanf(argv[2], "%d", &M);
-    }
-
-    graph* g = generate_new_connected_graph(N, M);
-
-    for (int i = 0; i < N; i++) {
+void initialize_graph(graph* g) {
+    DEBUG("Initializing graph with payload\n");
+    for (int i = 0; i < g->N; i++) {
         node* cur = elem_at(g->vertices, i);
 
         payload* data = malloc(sizeof(payload));
@@ -47,78 +31,83 @@ int main(int argc, char* argv[]) {
 
         cur->data = data;
     }
-    DEBUG("Added payload to each node\n");
+}
 
-    int keep_going = 1;
+void generate_random_field(graph* g) {
+    #pragma omp parallel for schedule(SCHEDULING_METHOD)
+    for (int i = 0; i < g->N; i++) {
+        node* cur = elem_at(g->vertices, i);
+        payload* data = cur->data;
 
-    while (keep_going) {
-        keep_going = 0;
+        if (!data->present)
+            continue;
 
-#pragma omp parallel for schedule(SCHEDULING_METHOD)
-        for (int i = 0; i < N; i++) {
-            node* cur = elem_at(g->vertices, i);
-            payload* data = cur->data;
+        data->r = randnum();
+    }
+}
 
-            if (!data->present)
-                continue;
+void decide_mis_entry(graph* g) {
+    #pragma omp parallel for schedule(SCHEDULING_METHOD)
+    for (int i = 0; i < g->N; i++) {
+        node* cur = elem_at(g->vertices, i);
+        payload* data = cur->data;
 
-            data->r = randnum();
+        if (!data->present)
+            continue;
+        
+        int enter = 1;
+        for (int i = 0; i < cur->degree; i++) {
+            node* neighbor = *((node**) elem_at(cur->neighbors, i));
+            payload* neighbor_data = neighbor->data;
+
+            if (data->r > neighbor_data->r) {
+                enter = 0;
+                break;
+            }
         }
 
-#pragma omp parallel for schedule(SCHEDULING_METHOD)
-        for (int i = 0; i < N; i++) {
-            node* cur = elem_at(g->vertices, i);
-            payload* data = cur->data;
+        if (enter) {
+            data->present = 0;
+            data->in_mis = 1;
+        }
+    }
+}
 
-            if (!data->present)
-                continue;
-            
-            int enter = 1;
+void remove_mis_adjacent_nodes(graph* g) {
+    #pragma omp parallel for schedule(SCHEDULING_METHOD)
+    for (int i = 0; i < g->N; i++) {
+        node* cur = elem_at(g->vertices, i);
+        payload* data = cur->data;
+
+        if (data->in_mis) {
             for (int i = 0; i < cur->degree; i++) {
                 node* neighbor = *((node**) elem_at(cur->neighbors, i));
                 payload* neighbor_data = neighbor->data;
 
-                if (data->r > neighbor_data->r) {
-                    enter = 0;
-                    break;
-                }
+                neighbor_data->present = 0;
             }
-
-            if (enter) {
-                data->present = 0;
-                data->in_mis = 1;
-            }
-        }
-
-#pragma omp parallel for schedule(SCHEDULING_METHOD)
-        for (int i = 0; i < N; i++) {
-            node* cur = elem_at(g->vertices, i);
-            payload* data = cur->data;
-
-            if (data->in_mis) {
-                for (int i = 0; i < cur->degree; i++) {
-                    node* neighbor = *((node**) elem_at(cur->neighbors, i));
-                    payload* neighbor_data = neighbor->data;
-
-                    neighbor_data->present = 0;
-                }
-            }
-        }
-
-#pragma omp parallel for schedule(SCHEDULING_METHOD)
-        for (int i = 0; i < N; i++) {
-            node* cur = elem_at(g->vertices, i);
-            payload* data = cur->data;
-
-            if (data->present)
-                keep_going = 1;
         }
     }
+}
 
-    // print_graph(g);
+int do_present_nodes_exist(graph* g) {
+    int keep_going = 0;
 
+    #pragma omp parallel for schedule(SCHEDULING_METHOD)
+    for (int i = 0; i < g->N; i++) {
+        node* cur = elem_at(g->vertices, i);
+        payload* data = cur->data;
+
+        if (data->present)
+            keep_going = 1;
+    }
+
+    return keep_going;
+}
+
+void verify_and_print_solution(graph* g) {
     printf("Elements in MIS: ");
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < g->N; i++) {
         node* cur = elem_at(g->vertices, i);
         payload* data = cur->data;
 
@@ -129,7 +118,7 @@ int main(int argc, char* argv[]) {
 
     printf("Verifying... ");
     int correct = 1;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < g->N; i++) {
         node* cur = elem_at(g->vertices, i);
         payload* data = cur->data;
 
@@ -151,4 +140,45 @@ int main(int argc, char* argv[]) {
         printf("Correct\n");
     else
         printf("Wrong\n");
+}
+
+/**
+ * Creates a MIS from a randomly generated graph in a distributed manner.
+ *
+ * Links:
+ *   - https://disco.ethz.ch/courses/podc_allstars/lecture/chapter7.pdf
+ *     See Algorithm 7.12 (Fast MIS-2)
+ */
+int main(int argc, char* argv[]) {
+    int N = 16;
+    int M = 64;
+
+    if (argc > 1) {
+        sscanf(argv[1], "%d", &N);
+        sscanf(argv[2], "%d", &M);
+    }
+
+    graph* g = generate_new_connected_graph(N, M);
+
+    initialize_graph(g);
+
+    int keep_going = 1;
+
+    while (keep_going) {
+        keep_going = 0;
+
+        generate_random_field(g);
+
+        decide_mis_entry(g);
+
+        remove_mis_adjacent_nodes(g);
+
+        keep_going = do_present_nodes_exist(g);
+    }
+
+    // print_graph(g);
+
+    verify_and_print_solution(g);
+
+    return 0;
 }
