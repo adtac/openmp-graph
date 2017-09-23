061e4f63c3be5cc25eb76856fc59f527bfe45c93
Adhityaa <c.adhityaa@gmail.com>
2017-09-23 13:46:24 +0530

    leader_elect_dp: Modularize code and split main into functions

--- src/leader_elect_dp.c
+++ src/leader_elect_dp.c
@@ -14,21 +14,12 @@ typedef struct {
 } processor;
 
 /**
- * Uses the distributed leader elect algorithm due to David Peleg (1990)
- * to determine a leader for any general network in a distributed fashion.
+ * initialize_graph - Initializes the graph with basic data
+ *
+ * @g: a pointer to the graph object
  */
-int main(int argc, char* argv[]) {
-    int N = 16;
-    int M = 64;
-
-    if (argc > 1) {
-        sscanf(argv[1], "%d", &N);
-        sscanf(argv[2], "%d", &M);
-    }
-
-    graph* g = generate_new_connected_graph(N, M);
-
-    for (int i = 0; i < N; i++) {
+void initialize_graph(graph* g) {
+    for (int i = 0; i < g->N; i++) {
         node* cur = elem_at(g->vertices, i);
 
         processor* p = malloc(sizeof(processor));
@@ -37,49 +28,47 @@ int main(int argc, char* argv[]) {
 
         cur->data = p;
     }
+}
 
-    int something_changed = 1;
-    int num_rounds = 0;
-    while (something_changed) {
-        num_rounds++;
-        something_changed = 0;
+void calculate_temporary_x(graph* g) {
+    #pragma omp parallel for schedule(SCHEDULING_METHOD)
+    for (int i = 0; i < g->N; i++) {
+        node* cur = elem_at(g->vertices, i);
+        processor* p = cur->data;
 
-#pragma omp parallel for schedule(SCHEDULING_METHOD)
-        for (int i = 0; i < N; i++) {
-            node* cur = elem_at(g->vertices, i);
-            processor* p = cur->data;
+        int new_x = p->x;
+        for (int j = 0; j < cur->degree; j++) {
+            node* neighbor = *((node**) elem_at(cur->neighbors, j));
+            processor* neighbor_p = neighbor->data;
 
-            int new_x = p->x;
-            for (int j = 0; j < cur->degree; j++) {
-                node* neighbor = *((node**) elem_at(cur->neighbors, j));
-                processor* neighbor_p = neighbor->data;
-
-                if (new_x < neighbor_p->x)
-                    new_x = neighbor_p->x;
-            }
-
-            p->new_x = new_x;
+            if (new_x < neighbor_p->x)
+                new_x = neighbor_p->x;
         }
 
-#pragma omp parallel for schedule(SCHEDULING_METHOD)
-        for (int i = 0; i < N; i++) {
-            node* cur = elem_at(g->vertices, i);
-            processor* p = cur->data;
+        p->new_x = new_x;
+    }
+}
 
-            if (p->new_x != p->x)
-                something_changed = 1;
+int propagate_temporary_x(graph* g) {
+    int something_changed = 0;
+    #pragma omp parallel for schedule(SCHEDULING_METHOD)
+    for (int i = 0; i < g->N; i++) {
+        node* cur = elem_at(g->vertices, i);
+        processor* p = cur->data;
 
-            p->x = p->new_x;
-        }
+        if (p->new_x != p->x)
+            something_changed = 1;
+
+        p->x = p->new_x;
     }
 
-    print_graph(g);
+    return something_changed;
+}
 
-    INFO("%d rounds elapsed\n", num_rounds);
-
+void verify_and_print_solution(graph* g) {
     int disagreement = 0;
     int leader = -1;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < g->N; i++) {
         node* cur = elem_at(g->vertices, i);
         processor* p = cur->data;
 
@@ -94,6 +83,40 @@ int main(int argc, char* argv[]) {
 
     if (!disagreement)
         INFO("Correct! All nodes have agreed to have %d as the leader.\n", leader);
-    
+}
+
+/**
+ * Uses the distributed leader elect algorithm due to David Peleg (1990)
+ * to determine a leader for any general network in a distributed fashion.
+ */
+int main(int argc, char* argv[]) {
+    int N = 16;
+    int M = 64;
+
+    if (argc > 1) {
+        sscanf(argv[1], "%d", &N);
+        sscanf(argv[2], "%d", &M);
+    }
+
+    graph* g = generate_new_connected_graph(N, M);
+
+    initialize_graph(g);
+
+    int something_changed = 1;
+    int num_rounds = 0;
+    while (something_changed) {
+        calculate_temporary_x(g);
+
+        something_changed = propagate_temporary_x(g);
+
+        num_rounds++;
+    }
+
+    INFO("%d rounds elapsed\n", num_rounds);
+
+    print_graph(g);
+
+    verify_and_print_solution(g);
+
     return 0;
 }
