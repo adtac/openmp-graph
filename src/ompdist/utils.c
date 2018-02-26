1621c486ea906db2e8e399536f599c2b76b489f4
Adhityaa <c.adhityaa@gmail.com>
2018-02-26 10:20:56 +0530

    utils: Use '-' to specify #iterations

--- src/ompdist/utils.c
+++ src/ompdist/utils.c
@@ -24,10 +24,7 @@ int input_through_argv(int argc, char* argv[]) {
     if (argc < 3)
         return 0;
 
-    if (argc == 3 && strcmp(argv[1], "-") == 0)
-        return 1;
-
-    if (argc == 4 && strcmp(argv[1], "+") == 0)
+    if (argc == 4 && strcmp(argv[1], "-") == 0)
         return 2;
 
     return 0;
